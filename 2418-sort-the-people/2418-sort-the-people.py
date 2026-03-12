0060a2c2b88f72c7b283b60c30b42e181d32c8f2
anh25037-creator <anh25037@gmail.com>
2026-03-12 21:59:07 +0700

    Time: 0 ms (100%), Space: 12.6 MB (95.26%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,5 +1,3 @@
 class Solution:
     def sortPeople(self, names, heights):
-        combined = list(zip(names, heights))
-        combined.sort(key=lambda x: x[1], reverse=True)
-        return [person[0] for person in combined]
+        return [x for _,x in sorted(zip(heights,names), reverse=True)]
