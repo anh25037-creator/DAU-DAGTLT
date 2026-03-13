05e0a673994ec0c04eec4cb6132ab7b9cf893fbb
anh25037-creator <anh25037@gmail.com>
2026-03-13 13:54:06 +0700

    Time: 14 ms (18.25%), Space: 13.7 MB (41.35%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,5 +4,8 @@ class Solution:
         
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[i], nums[k] = nums[k], nums[i]
+                nums[k] = nums[i]
                 k += 1
+        
+        for i in range(k, len(nums)):
+            nums[i] = 0
