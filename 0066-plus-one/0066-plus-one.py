03f70789849caa699df716db3730e900b70781da
anh25037-creator <anh25037@gmail.com>
2026-05-08 10:01:22 +0700

    Time: 0 ms (100%), Space: 12.3 MB (54.47%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,11 +1,22 @@
-class Solution:
+class Solution(object):
     def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+
         n = len(digits)
 
+        # duyệt từ cuối mảng về đầu
         for i in range(n - 1, -1, -1):
+
+            # nếu nhỏ hơn 9 → cộng 1 rồi return luôn
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
+
+            # nếu là 9 → thành 0 (và carry sang trái)
             digits[i] = 0
 
+        # nếu tất cả đều là 9 (ví dụ 999)
         return [1] + digits
