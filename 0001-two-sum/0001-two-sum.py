51f2ecd2eccb91bfb59a504c3caf2063b6d4ea9e
anh25037-creator <anh25037@gmail.com>
2026-05-16 22:25:59 +0700

    Time: 3 ms (53.62%), Space: 13.2 MB (52.7%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,23 @@
 class Solution:
     def twoSum(self, nums, target):
-        seen = {}  # lưu: value -> index
         
+        # Dictionary dùng để lưu:
+        # số trong mảng -> vị trí(index) của số đó
+        seen = {}
+        
+        # Duyệt từng phần tử trong mảng
         for i in range(len(nums)):
+            
+            # Tính số cần tìm để cộng lại bằng target
             need = target - nums[i]
             
+            # Nếu số cần tìm đã xuất hiện trước đó
             if need in seen:
+                
+                # Trả về vị trí của:
+                # - số đã lưu trước đó
+                # - số hiện tại
                 return [seen[need], i]
             
+            # Lưu số hiện tại và vị trí của nó vào dictionary
             seen[nums[i]] = i
