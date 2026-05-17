65098555c6cbbf55de86c392801c9ef8f76d86e3
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:50:03 +0700

    Time: 0 ms (100%), Space: 12.3 MB (88.78%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.py
@@ -1,11 +1,24 @@
 class Solution:
     def countElements(self, nums):
+
+        # Tìm phần tử nhỏ nhất trong mảng
         mn = min(nums)
+
+        # Tìm phần tử lớn nhất trong mảng
         mx = max(nums)
+
+        # Biến đếm kết quả
         count = 0
-        
+
+        # Duyệt từng phần tử trong mảng
         for x in nums:
+
+            # Nếu x lớn hơn số nhỏ nhất
+            # và nhỏ hơn số lớn nhất
             if mn < x < mx:
+
+                # Tăng biến đếm
                 count += 1
-                
+
+        # Trả về kết quả
         return count
