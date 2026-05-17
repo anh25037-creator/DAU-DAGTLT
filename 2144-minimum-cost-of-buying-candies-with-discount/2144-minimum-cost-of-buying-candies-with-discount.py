a7d0b97e52d96871b3ab45fbc679e682fe9d4b0a
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:46:50 +0700

    Time: 0 ms (100%), Space: 12.4 MB (48.41%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,10 +1,20 @@
 class Solution:
     def minimumCost(self, cost):
+
+        # Sắp xếp giảm dần
         cost.sort(reverse=True)
+
+        # Tổng tiền cần trả
         total = 0
-        
+
+        # Duyệt từng viên kẹo
         for i in range(len(cost)):
-            if (i + 1) % 3 != 0:   # cứ viên thứ 3 thì miễn phí
+
+            # Mỗi viên thứ 3 sẽ được miễn phí
+            if (i + 1) % 3 != 0:
+
+                # Cộng tiền
                 total += cost[i]
-                
+
+        # Trả về kết quả
         return total
