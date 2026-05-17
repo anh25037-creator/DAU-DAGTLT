acab4b74af45c22a10ece52aa4569e150dd1ab5f
anh25037-creator <anh25037@gmail.com>
2026-05-17 20:46:29 +0700

    Time: 4 ms (42.75%), Space: 12.5 MB (25.95%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.py
+++ 2293-min-max-game/2293-min-max-game.py
@@ -1,24 +1,28 @@
-class Solution(object):
+class Solution:
     def minMaxGame(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
 
+        # Lặp lại quá trình cho đến khi mảng còn 1 phần tử
         while len(nums) > 1:
 
-            newNums = [0] * (len(nums) // 2)
+            # Mảng mới sau mỗi lần xử lý cặp phần tử
+            newNums = []
 
-            for i in range(len(newNums)):
+            # Duyệt theo từng cặp (2*i, 2*i+1)
+            # Vì mỗi vòng sẽ giảm còn n/2 phần tử
+            for i in range(len(nums) // 2):
 
-                # index chẵn → lấy min
+                # Nếu index i của newNums là chẵn
+                # → lấy min của cặp tương ứng trong nums
                 if i % 2 == 0:
-                    newNums[i] = min(nums[2 * i], nums[2 * i + 1])
+                    newNums.append(min(nums[2*i], nums[2*i + 1]))
 
-                # index lẻ → lấy max
+                # Nếu index i là lẻ
+                # → lấy max của cặp tương ứng trong nums
                 else:
-                    newNums[i] = max(nums[2 * i], nums[2 * i + 1])
+                    newNums.append(max(nums[2*i], nums[2*i + 1]))
 
+            # Cập nhật nums thành mảng mới
             nums = newNums
 
+        # Khi chỉ còn 1 phần tử → trả kết quả
         return nums[0]
