bf0e5692c16355b64fd8c5601f8f2ccf6f945d8f
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:47:23 +0700

    Time: 0 ms (100%), Space: 12.3 MB (91.89%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,12 +1,15 @@
 class Solution:
     def pivotInteger(self, n):
+        
+        # tổng từ 1 đến n
         total = n * (n + 1) // 2
         
-        for x in range(1, n + 1):
-            left = x * (x + 1) // 2
-            right = total - left + x
-            
-            if left == right:
-                return x
+        # lấy căn bậc 2 của total
+        x = int(total ** 0.5)
         
+        # kiểm tra xem có phải số chính phương không
+        if x * x == total:
+            return x
+        
+        # nếu không tồn tại pivot
         return -1
