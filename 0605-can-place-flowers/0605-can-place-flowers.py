cb390d6fad135e77625cf952be4127d1300f7bb8
anh25037-creator <anh25037@gmail.com>
2026-05-17 13:44:05 +0700

    Time: 9 ms (68.7%), Space: 13 MB (72.85%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,14 +1,27 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed, n):
+        
+        # Đếm số hoa có thể trồng thêm
         count = 0
         
+        # Duyệt từng vị trí
         for i in range(len(flowerbed)):
-            if flowerbed[i] == 0:
-                left = (i == 0) or (flowerbed[i - 1] == 0)
-                right = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+            
+            # Kiểm tra ô bên trái
+            left_empty = (i == 0 or flowerbed[i - 1] == 0)
+            
+            # Kiểm tra ô bên phải
+            right_empty = (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
+            
+            # Nếu vị trí hiện tại trống
+            # và 2 bên đều trống
+            if flowerbed[i] == 0 and left_empty and right_empty:
                 
-                if left and right:
-                    flowerbed[i] = 1
-                    count += 1
+                # Trồng hoa tại đây
+                flowerbed[i] = 1
+                
+                # Tăng số lượng hoa đã trồng
+                count += 1
         
+        # Nếu trồng được ít nhất n bông hoa
         return count >= n
