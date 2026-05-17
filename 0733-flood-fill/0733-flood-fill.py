0247bfe706ce141dfafecfd05a6e6a6babd755af
anh25037-creator <anh25037@gmail.com>
2026-05-17 20:39:30 +0700

    Time: 0 ms (100%), Space: 12.5 MB (62.41%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,41 +1,36 @@
-class Solution(object):
+class Solution:
     def floodFill(self, image, sr, sc, color):
-        """
-        :type image: List[List[int]]
-        :type sr: int
-        :type sc: int
-        :type color: int
-        :rtype: List[List[int]]
-        """
 
-        rows = len(image)
-        cols = len(image[0])
+        # màu ban đầu tại điểm xuất phát
+        old_color = image[sr][sc]
 
-        original = image[sr][sc]
-
-        # nếu màu mới giống màu cũ
-        if original == color:
+        # nếu màu mới giống màu cũ → không cần đổi
+        if old_color == color:
             return image
 
+        # số hàng và cột
+        m, n = len(image), len(image[0])
+
+        # DFS để tô màu
         def dfs(r, c):
-
-            # ngoài biên
-            if r < 0 or r >= rows or c < 0 or c >= cols:
+            # nếu ra ngoài biên → dừng
+            if r < 0 or r >= m or c < 0 or c >= n:
                 return
 
-            # khác màu gốc
-            if image[r][c] != original:
+            # nếu không đúng màu cũ → dừng
+            if image[r][c] != old_color:
                 return
 
-            # đổi màu
+            # tô màu mới
             image[r][c] = color
 
-            # 4 hướng
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+            # lan sang 4 hướng
+            dfs(r + 1, c)  # xuống
+            dfs(r - 1, c)  # lên
+            dfs(r, c + 1)  # phải
+            dfs(r, c - 1)  # trái
 
+        # bắt đầu từ điểm xuất phát
         dfs(sr, sc)
 
         return image
