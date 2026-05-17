063200ed2bbe9eb6fe1b5fcc1cc84dc7aa621a67
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:48:40 +0700

    Time: 0 ms (100%), Space: 12.2 MB (94.87%) - LeetHub

--- 1694-reformat-phone-number/1694-reformat-phone-number.py
+++ 1694-reformat-phone-number/1694-reformat-phone-number.py
@@ -1,24 +1,49 @@
 class Solution:
     def reformatNumber(self, number):
+
+        # Chuỗi chỉ chứa các chữ số
         digits = ""
-        
-        # Lấy các chữ số
-        for c in number:
-            if c.isdigit():
-                digits += c
 
-        res = []
+        # Duyệt từng ký tự trong number
+        for ch in number:
 
-        while len(digits) > 0:
-            if len(digits) > 4:
-                res.append(digits[:3])
-                digits = digits[3:]
-            elif len(digits) == 4:
-                res.append(digits[:2])
-                res.append(digits[2:])
-                break
-            else:
-                res.append(digits)
-                break
+            # Nếu là số thì thêm vào digits
+            if ch.isdigit():
+                digits += ch
 
-        return "-".join(res)
+        # Chuỗi kết quả cuối cùng
+        result = ""
+
+        # Biến i để đánh dấu vị trí đang xét
+        i = 0
+
+        # Lặp khi số chữ số còn lại lớn hơn 4
+        while len(digits) - i > 4:
+
+            # Lấy 3 chữ số từ vị trí i
+            result += digits[i:i+3]
+
+            # Thêm dấu "-"
+            result += "-"
+
+            # Di chuyển sang nhóm tiếp theo
+            i += 3
+
+        # Tính số chữ số còn lại
+        remain = len(digits) - i
+
+        # Nếu còn đúng 4 số
+        if remain == 4:
+
+            # Chia thành 2 nhóm 2 số
+            result += digits[i:i+2]
+            result += "-"
+            result += digits[i+2:i+4]
+
+        else:
+            # Nếu còn 2 hoặc 3 số
+            # Thêm trực tiếp vào kết quả
+            result += digits[i:]
+
+        # Trả về chuỗi đã format
+        return result
