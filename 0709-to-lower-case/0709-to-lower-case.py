ad2a65ab3545606f14a20ce113eda9ecf51adbdb
anh25037-creator <anh25037@gmail.com>
2026-05-17 14:03:33 +0700

    Time: 0 ms (100%), Space: 12.4 MB (17.84%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,11 +1,5 @@
 class Solution:
     def toLowerCase(self, s):
-        result = ""
-        
-        for c in s:
-            if 'A' <= c <= 'Z':
-                result += chr(ord(c) + 32)
-            else:
-                result += c
-                
-        return result
+
+        # Chuyển toàn bộ ký tự thành chữ thường
+        return s.lower()
