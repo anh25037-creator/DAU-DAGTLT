bb68552f9418079d4a45e8803933a4dd2da38c3d
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:40:28 +0700

    Time: 38 ms (63.95%), Space: 12.7 MB (34.01%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.py
@@ -1,14 +1,31 @@
 class Solution:
     def checkValid(self, matrix):
+
+        # Kích thước ma trận
         n = len(matrix)
-        target = set(range(1, n + 1))
-        
-        for i in range(n):
-            if set(matrix[i]) != target:
+
+        # Tập hợp chuẩn chứa các số từ 1 -> n
+        valid = set(range(1, n + 1))
+
+        # Kiểm tra từng hàng
+        for row in matrix:
+
+            # Nếu hàng không chứa đủ các số từ 1 -> n
+            if set(row) != valid:
                 return False
-            
-            col = set(matrix[j][i] for j in range(n))
-            if col != target:
+
+        # Kiểm tra từng cột
+        for col in range(n):
+
+            # Tạo danh sách chứa các phần tử của cột
+            column = []
+
+            for row in range(n):
+                column.append(matrix[row][col])
+
+            # Nếu cột không hợp lệ
+            if set(column) != valid:
                 return False
-        
+
+        # Nếu tất cả đều hợp lệ
         return True
