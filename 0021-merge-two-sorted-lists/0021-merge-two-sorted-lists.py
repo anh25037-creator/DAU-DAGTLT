d7060441a8c3c91e467ffc2634fe422760450f0c
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:53:29 +0700

    Time: 0 ms (100%), Space: 12.5 MB (48.2%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,28 +1,26 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
     def mergeTwoLists(self, list1, list2):
-        dummy = ListNode(0)  # node giả
+        # node giả giúp dễ thao tác
+        dummy = ListNode(0)
         curr = dummy
-        
+
+        # duyệt khi cả 2 list còn node
         while list1 and list2:
-            if list1.val < list2.val:
-                curr.next = list1
-                list1 = list1.next
+            # chọn node có giá trị nhỏ hơn
+            if list1.val <= list2.val:
+                curr.next = list1      # nối list1 vào kết quả
+                list1 = list1.next     # tiến list1
             else:
-                curr.next = list2
-                list2 = list2.next
-            
-            curr = curr.next
-        
-        # nối phần còn lại
+                curr.next = list2      # nối list2 vào kết quả
+                list2 = list2.next     # tiến list2
+
+            curr = curr.next           # tiến con trỏ kết quả
+
+        # nối phần còn lại (vì 1 trong 2 list có thể chưa hết)
         if list1:
             curr.next = list1
         else:
             curr.next = list2
-        
+
+        # bỏ node giả, trả về head thật
         return dummy.next
