66087d0a8015d3217b35b474c98e39efefcb0ec2
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:03:04 +0700

    Time: 1 ms (80.38%), Space: 12.2 MB (94.78%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,9 +1,40 @@
-import re
-from collections import Counter
-
 class Solution:
     def mostCommonWord(self, paragraph, banned):
-        words = re.findall(r'\w+', paragraph.lower())
+
+        # Chuyển tất cả ký tự thành chữ thường
+        paragraph = paragraph.lower()
+
+        # Thay các dấu câu bằng khoảng trắng
+        for ch in "!?',;.":
+            paragraph = paragraph.replace(ch, " ")
+
+        # Tách các từ
+        words = paragraph.split()
+
+        # Chuyển banned thành set để tìm nhanh hơn
         banned_set = set(banned)
-        count = Counter(word for word in words if word not in banned_set)
-        return count.most_common(1)[0][0]
+
+        # Dictionary đếm số lần xuất hiện
+        count = {}
+
+        # Đếm tần suất các từ không bị cấm
+        for word in words:
+
+            # Nếu từ không nằm trong banned
+            if word not in banned_set:
+
+                # Tăng số lần xuất hiện
+                count[word] = count.get(word, 0) + 1
+
+        # Tìm từ xuất hiện nhiều nhất
+        answer = ""
+        max_count = 0
+
+        for word in count:
+
+            if count[word] > max_count:
+                max_count = count[word]
+                answer = word
+
+        # Trả về kết quả
+        return answer
