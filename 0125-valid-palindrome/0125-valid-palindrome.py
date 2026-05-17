26d760b30aedcc2efe3cea226d051c74e14f979b
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:18:22 +0700

    Time: 279 ms (5.03%), Space: 13.1 MB (49.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,9 +1,17 @@
 class Solution:
     def isPalindrome(self, s):
+        
+        # tạo chuỗi mới sau khi lọc ký tự hợp lệ
         new = ""
-
+        
+        # duyệt từng ký tự trong chuỗi ban đầu
         for c in s:
+            
+            # chỉ giữ lại ký tự chữ hoặc số
             if c.isalnum():
+                
+                # chuyển về chữ thường và thêm vào chuỗi mới
                 new += c.lower()
-
+        
+        # so sánh chuỗi với bản đảo ngược của nó
         return new == new[::-1]
