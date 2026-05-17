61990e0a9553f63fb77add3add434dda07c46845
anh25037-creator <anh25037@gmail.com>
2026-05-17 13:39:25 +0700

    Time: 0 ms (100%), Space: 12.3 MB (96.43%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,10 +1,24 @@
 class Solution:
     def longestCommonPrefix(self, strs):
+        
+        # Nếu mảng rỗng
         if not strs:
             return ""
-
-        for i, ch in enumerate(strs[0]):
-            for s in strs[1:]:
-                if i >= len(s) or s[i] != ch:
-                    return strs[0][:i]
-        return strs[0]
+        
+        # Lấy chuỗi đầu tiên làm prefix ban đầu
+        prefix = strs[0]
+        
+        # Duyệt các chuỗi còn lại
+        for s in strs[1:]:
+            
+            # Nếu chuỗi hiện tại không bắt đầu bằng prefix
+            while not s.startswith(prefix):
+                
+                # Xóa ký tự cuối của prefix
+                prefix = prefix[:-1]
+                
+                # Nếu prefix rỗng
+                if prefix == "":
+                    return ""
+        
+        return prefix
