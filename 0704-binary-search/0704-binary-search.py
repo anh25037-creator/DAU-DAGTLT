5703e4ca4609512c26069e1225af3b35a89ccb45
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:28:23 +0700

    Time: 0 ms (100%), Space: 13.3 MB (72.07%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,16 +1,26 @@
-class Solution(object):
+class Solution:
     def search(self, nums, target):
-        left = 0
-        right = len(nums) - 1
-
+        
+        # con trỏ trái và phải
+        left, right = 0, len(nums) - 1
+        
+        # lặp khi còn khoảng tìm kiếm
         while left <= right:
+            
+            # tìm vị trí giữa
             mid = (left + right) // 2
-
+            
+            # nếu tìm thấy target
             if nums[mid] == target:
                 return mid
+            
+            # nếu giá trị giữa nhỏ hơn target → tìm bên phải
             elif nums[mid] < target:
                 left = mid + 1
+            
+            # nếu lớn hơn target → tìm bên trái
             else:
                 right = mid - 1
-
+        
+        # không tìm thấy
         return -1
