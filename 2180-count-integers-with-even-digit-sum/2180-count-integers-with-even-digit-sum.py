3bb168a02a5614e86eb52c1087b2ae620138e84d
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:00:11 +0700

    Time: 7 ms (80.51%), Space: 12.4 MB (18.75%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.py
@@ -2,9 +2,18 @@ class Solution:
     def countEven(self, num):
         count = 0
         
-        for i in range(1, num + 1):
-            s = sum(int(d) for d in str(i))
+        # duyệt tất cả số từ 1 đến num
+        for x in range(1, num + 1):
+            s = 0
+            
+            # tính tổng chữ số của x
+            temp = x
+            while temp > 0:
+                s += temp % 10   # lấy chữ số cuối
+                temp //= 10      # bỏ chữ số cuối
+            
+            # kiểm tra tổng chữ số có chẵn không
             if s % 2 == 0:
                 count += 1
-                
+        
         return count
