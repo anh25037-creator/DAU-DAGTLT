71347e065ce52c2e723dcc35f31ce106de7885ce
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:22:25 +0700

    Time: 0 ms (100%), Space: 12.5 MB (17.27%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,13 +1,27 @@
 class Solution:
     def removeElement(self, nums, val):
-        i = 0
+        
+        # n là kích thước hiện tại của mảng hợp lệ
         n = len(nums)
-
+        
+        # i là con trỏ duyệt mảng
+        i = 0
+        
+        # duyệt đến khi i vượt qua n
         while i < n:
+            
+            # nếu gặp phần tử cần xóa
             if nums[i] == val:
-                nums[i] = nums[n-1]
+                
+                # thay bằng phần tử cuối cùng trong vùng hợp lệ
+                nums[i] = nums[n - 1]
+                
+                # giảm kích thước vùng hợp lệ
                 n -= 1
+            
             else:
+                # chỉ tăng i khi phần tử không bị xóa
                 i += 1
-
+        
+        # số phần tử còn lại
         return n
