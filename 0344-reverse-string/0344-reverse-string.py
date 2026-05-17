113ecfd5dd95252036fbe85e20c6a180cc6b766f
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:24:39 +0700

    Time: 3 ms (56.12%), Space: 19.8 MB (75.78%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,9 +1,18 @@
 class Solution:
     def reverseString(self, s):
+        
+        # con trỏ trái bắt đầu từ đầu mảng
         left = 0
+        
+        # con trỏ phải bắt đầu từ cuối mảng
         right = len(s) - 1
-
+        
+        # duyệt cho đến khi 2 con trỏ gặp nhau
         while left < right:
+            
+            # hoán đổi ký tự ở 2 đầu
             s[left], s[right] = s[right], s[left]
+            
+            # di chuyển con trỏ vào giữa
             left += 1
             right -= 1
