7af98588821d302cf9671b35384a1b1d4631e980
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:32:55 +0700

    Time: 0 ms (100%), Space: 12.3 MB (89.38%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,19 +1,30 @@
 class Solution:
     def mySqrt(self, x):
-        if x < 2:
-            return x
         
-        left = 1
-        right = x
+        # khoảng tìm kiếm từ 0 đến x
+        left, right = 0, x
         
+        # đáp án tạm thời
+        ans = 0
+        
+        # binary search
         while left <= right:
+            
+            # chọn giữa
             mid = (left + right) // 2
             
-            if mid * mid == x:
-                return mid
-            elif mid * mid < x:
+            # nếu bình phương mid nhỏ hơn hoặc bằng x
+            if mid * mid <= x:
+                
+                # cập nhật đáp án tốt nhất hiện tại
+                ans = mid
+                
+                # thử tìm số lớn hơn
                 left = mid + 1
+            
             else:
+                # mid quá lớn → giảm xuống
                 right = mid - 1
         
-        return right
+        # trả về căn bậc 2 làm tròn xuống
+        return ans
