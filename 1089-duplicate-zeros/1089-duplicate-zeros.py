6b8592c1e9c3808501dbe3b779c0110a656c11c9
anh25037-creator <anh25037@gmail.com>
2026-05-17 14:55:51 +0700

    Time: 7 ms (28.54%), Space: 12.6 MB (85.37%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -1,9 +1,22 @@
 class Solution:
     def duplicateZeros(self, arr):
+        
+        # Duyệt từng phần tử trong mảng
         i = 0
+        
         while i < len(arr):
+
+            # Nếu gặp số 0
             if arr[i] == 0:
+
+                # Chèn thêm một số 0 vào vị trí hiện tại
                 arr.insert(i, 0)
+
+                # Xóa phần tử cuối để giữ nguyên độ dài mảng
                 arr.pop()
+
+                # Bỏ qua số 0 vừa chèn thêm
                 i += 1
+
+            # Sang vị trí tiếp theo
             i += 1
