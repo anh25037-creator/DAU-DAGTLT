02d0c6e378978779ce0d2426d7b173ce18bb5908
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:54:44 +0700

    Time: 34 ms (74.18%), Space: 19.5 MB (29.78%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,19 +1,20 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
 class Solution:
     def hasCycle(self, head):
-        slow = head
-        fast = head
-        
+        # nếu list rỗng hoặc chỉ có 1 node thì không có cycle
+        if not head or not head.next:
+            return False
+
+        slow = head      # đi từng bước
+        fast = head      # đi hai bước
+
+        # duyệt đến khi fast hoặc fast.next hết
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            
+            slow = slow.next          # đi 1 bước
+            fast = fast.next.next     # đi 2 bước
+
+            # nếu gặp nhau → có cycle
             if slow == fast:
                 return True
-        
+
+        # đi đến cuối mà không gặp → không có cycle
         return False
