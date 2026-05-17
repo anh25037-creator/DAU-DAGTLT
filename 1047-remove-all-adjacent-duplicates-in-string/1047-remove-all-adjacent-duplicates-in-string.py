391934d837f5199645bcbfd5554c23f8a7247cdd
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:26:32 +0700

    Time: 35 ms (85.85%), Space: 13.6 MB (32.64%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -1,11 +1,21 @@
 class Solution:
     def removeDuplicates(self, s):
+        
+        # stack để lưu kết quả tạm thời
         stack = []
-
+        
+        # duyệt từng ký tự trong chuỗi
         for c in s:
+            
+            # nếu stack không rỗng và ký tự hiện tại trùng với ký tự trên đỉnh stack
             if stack and stack[-1] == c:
+                
+                # xóa ký tự trùng (pop)
                 stack.pop()
+            
             else:
+                # nếu không trùng thì thêm vào stack
                 stack.append(c)
-
+        
+        # ghép lại thành chuỗi kết quả
         return "".join(stack)
