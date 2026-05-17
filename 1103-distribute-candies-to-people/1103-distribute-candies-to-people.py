f0a52084c1aae5ad3122ded7f8ef6baa384f5c93
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:07:17 +0700

    Time: 4 ms (76.84%), Space: 12.4 MB (58.95%) - LeetHub

--- 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
+++ 1103-distribute-candies-to-people/1103-distribute-candies-to-people.py
@@ -1,18 +1,39 @@
 class Solution:
     def distributeCandies(self, candies, num_people):
+
+        # Tạo mảng kết quả ban đầu toàn 0
         result = [0] * num_people
+
+        # Số kẹo sẽ phát ở lượt hiện tại
         give = 1
-        index = 0
-        
+
+        # Vị trí người nhận
+        i = 0
+
+        # Khi vẫn còn kẹo
         while candies > 0:
-            if candies >= give:
-                result[index] += give
-                candies -= give
+
+            # Nếu số kẹo còn lại nhỏ hơn số cần phát
+            if candies < give:
+
+                # Đưa hết số kẹo còn lại
+                result[i] += candies
+
+                # Hết kẹo
+                candies = 0
+
             else:
-                result[index] += candies
-                break
-            
+                # Phát kẹo bình thường
+                result[i] += give
+
+                # Trừ số kẹo đã phát
+                candies -= give
+
+            # Tăng số kẹo cho lượt sau
             give += 1
-            index = (index + 1) % num_people
-        
+
+            # Chuyển sang người tiếp theo
+            i = (i + 1) % num_people
+
+        # Trả về kết quả
         return result
