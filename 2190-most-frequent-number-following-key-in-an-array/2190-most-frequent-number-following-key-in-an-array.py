43bbb48ff5145bf26c9edf50300b166d10a2f9e8
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:04:26 +0700

    Time: 5 ms (10.67%), Space: 12.4 MB (64%) - LeetHub

--- 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.py
+++ 2190-most-frequent-number-following-key-in-an-array/2190-most-frequent-number-following-key-in-an-array.py
@@ -1,10 +1,23 @@
 class Solution:
+    # tìm số đứng ngay sau key nhiều lần nhất
     def mostFrequent(self, nums, key):
+        
+        # dictionary để đếm số lần xuất hiện của các target
         count = {}
         
+        # duyệt mảng đến phần tử kế cuối (vì dùng i+1)
         for i in range(len(nums) - 1):
+            
+            # nếu gặp key tại vị trí i
             if nums[i] == key:
+                
+                # lấy phần tử ngay sau key
                 target = nums[i + 1]
+                
+                # tăng số lần xuất hiện của target
+                # nếu chưa có thì mặc định là 0
                 count[target] = count.get(target, 0) + 1
         
+        # tìm key có giá trị lớn nhất trong dictionary count
+        # (trả về target xuất hiện nhiều nhất sau key)
         return max(count, key=count.get)
