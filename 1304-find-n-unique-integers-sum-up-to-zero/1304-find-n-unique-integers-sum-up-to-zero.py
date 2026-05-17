f5f954b6b1665b068e58dc026d192c81a8424c28
anh25037-creator <anh25037@gmail.com>
2026-05-17 14:56:50 +0700

    Time: 0 ms (100%), Space: 12.3 MB (81.96%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.py
@@ -1,12 +1,17 @@
 class Solution:
     def sumZero(self, n):
+
+        # Mảng kết quả
         result = []
-        
+
+        # Thêm các cặp số đối nhau
         for i in range(1, n // 2 + 1):
-            result.append(i)
-            result.append(-i)
-        
+            result.append(i)    # số dương
+            result.append(-i)   # số âm
+
+        # Nếu n lẻ thì thêm số 0
         if n % 2 == 1:
             result.append(0)
-        
+
+        # Trả về kết quả
         return result
