7cc6556c94138760c3ff9286038492f6d4177b18
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:31:49 +0700

    Time: 0 ms (100%), Space: 12.8 MB (65.55%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,16 +1,27 @@
 class Solution:
     def searchInsert(self, nums, target):
-        left = 0
-        right = len(nums) - 1
         
+        # con trỏ trái và phải
+        left, right = 0, len(nums) - 1
+        
+        # binary search
         while left <= right:
+            
+            # tìm vị trí giữa
             mid = (left + right) // 2
             
+            # nếu tìm thấy target → trả luôn index
             if nums[mid] == target:
                 return mid
+            
+            # nếu mid nhỏ hơn target → tìm bên phải
             elif nums[mid] < target:
                 left = mid + 1
+            
+            # nếu mid lớn hơn target → tìm bên trái
             else:
                 right = mid - 1
         
+        # nếu không tìm thấy:
+        # left là vị trí cần chèn
         return left
