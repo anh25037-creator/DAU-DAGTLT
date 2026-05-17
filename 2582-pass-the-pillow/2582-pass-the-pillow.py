cc4ea681da61092513489a0a5dceb39652c2a328
anh25037-creator <anh25037@gmail.com>
2026-05-17 20:45:27 +0700

    Time: 0 ms (100%), Space: 12.4 MB (57.14%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -6,14 +6,22 @@ class Solution(object):
         :rtype: int
         """
 
+        # Chu kỳ đầy đủ của chuyển động:
+        # đi từ 1 → n rồi quay ngược lại n → 1
+        # tổng độ dài = 2 * (n - 1)
         cycle = 2 * (n - 1)
 
+        # Lấy thời gian thực tế trong 1 chu kỳ
+        # giúp tránh xử lý các vòng lặp dư thừa
         t = time % cycle
 
-        # đang đi tới
+        # Trường hợp đang đi xuôi (1 → n)
+        # trong đoạn đầu của chu kỳ
         if t < n:
+            # vị trí bắt đầu từ 1 nên +1
             return t + 1
 
-        # đang đi lui
+        # Trường hợp đang đi ngược (n → 1)
+        # công thức biến đổi từ chuỗi quay ngược
         return 2 * n - t - 1
         
