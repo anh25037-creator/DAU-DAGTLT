f36eb9c35d3bb2b01218562256b0349414ae9718
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:53:30 +0700

    Time: 1113 ms (11.88%), Space: 15.1 MB (78.22%) - LeetHub

--- 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.py
@@ -1,13 +1,22 @@
 class Solution:
     def countBalls(self, lowLimit, highLimit):
+
+        # Dictionary lưu số lượng bóng trong từng hộp
         box = {}
 
+        # Duyệt các số từ lowLimit đến highLimit
         for i in range(lowLimit, highLimit + 1):
-            s = sum(int(d) for d in str(i))  # tổng chữ số
-            
+
+            # Tính tổng các chữ số của i
+            s = sum(int(d) for d in str(i))
+
+            # Nếu hộp đã tồn tại
             if s in box:
                 box[s] += 1
+
+            # Nếu hộp chưa tồn tại
             else:
                 box[s] = 1
 
+        # Trả về số bóng lớn nhất trong các hộp
         return max(box.values())
