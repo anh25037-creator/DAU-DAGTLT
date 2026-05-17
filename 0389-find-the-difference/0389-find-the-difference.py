1f728db239650284d54ccda54f59cb5051ae63c7
anh25037-creator <anh25037@gmail.com>
2026-05-17 13:57:24 +0700

    Time: 3 ms (64.24%), Space: 12.3 MB (66.79%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,3 +1,20 @@
 class Solution:
     def findTheDifference(self, s, t):
-        return chr(sum(map(ord, t)) - sum(map(ord, s)))
+
+        # Dictionary để đếm ký tự
+        count = {}
+
+        # Đếm số lần xuất hiện trong s
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        # Duyệt chuỗi t
+        for ch in t:
+
+            # Nếu ký tự chưa có
+            # hoặc đã dùng hết
+            if ch not in count or count[ch] == 0:
+                return ch
+
+            # Giảm số lần xuất hiện
+            count[ch] -= 1
