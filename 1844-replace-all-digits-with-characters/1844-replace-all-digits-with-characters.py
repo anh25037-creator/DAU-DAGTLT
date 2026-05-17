f244ed0630bc7d2f75b1869cfd47e196257d0fbc
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:14:29 +0700

    Time: 3 ms (23.91%), Space: 12.4 MB (57.97%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.py
@@ -1,8 +1,20 @@
 class Solution:
     def replaceDigits(self, s):
+
+        # Chuyển chuỗi thành list để dễ thay đổi ký tự
         s = list(s)
-        
+
+        # Duyệt các vị trí lẻ
         for i in range(1, len(s), 2):
-            s[i] = chr(ord(s[i-1]) + int(s[i]))
-        
+
+            # Ký tự đứng trước
+            ch = s[i - 1]
+
+            # Số cần dịch
+            x = int(s[i])
+
+            # Dịch ký tự bằng mã ASCII
+            s[i] = chr(ord(ch) + x)
+
+        # Ghép list thành chuỗi
         return "".join(s)
