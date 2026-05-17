cd363983c86ea410465694495f006b40bce83584
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:20:13 +0700

    Time: 0 ms (100%), Space: 12.2 MB (88.46%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,11 +1,22 @@
 class Solution:
     def timeRequiredToBuy(self, tickets, k):
+
+        # Biến lưu tổng thời gian
         time = 0
-        
+
+        # Duyệt từng người trong hàng
         for i in range(len(tickets)):
+
+            # Nếu người đứng trước hoặc chính k
             if i <= k:
+
+                # Người này có thể mua tối đa tickets[k] vé
                 time += min(tickets[i], tickets[k])
+
             else:
+                # Người đứng sau k
+                # Chỉ mua tối đa tickets[k] - 1 vé
                 time += min(tickets[i], tickets[k] - 1)
-                
+
+        # Trả về tổng thời gian
         return time
