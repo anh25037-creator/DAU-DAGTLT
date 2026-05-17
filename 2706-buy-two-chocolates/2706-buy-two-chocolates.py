7fd7577c5fcd8c8835cc25133ed6afd0f2bf5ac0
anh25037-creator <anh25037@gmail.com>
2026-05-17 20:44:20 +0700

    Time: 4 ms (32.8%), Space: 12.3 MB (91.01%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -6,15 +6,19 @@ class Solution(object):
         :rtype: int
         """
 
-        # tìm 2 giá nhỏ nhất
+        # Sắp xếp mảng giá theo thứ tự tăng dần
+        # để lấy được 2 món rẻ nhất dễ dàng
         prices.sort()
 
+        # Tổng chi phí của 2 viên chocolate rẻ nhất
         cost = prices[0] + prices[1]
 
-        # đủ tiền mua
+        # Nếu đủ tiền mua 2 viên chocolate
         if cost <= money:
+            # trả về số tiền còn lại sau khi mua
             return money - cost
 
-        # không đủ tiền
+        # Nếu không đủ tiền mua 2 viên
+        # thì không mua được → trả lại toàn bộ tiền ban đầu
         return money
         
