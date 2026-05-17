a9799e028b1122a7ccf5d99635907ed36d94b653
anh25037-creator <anh25037@gmail.com>
2026-05-17 20:06:28 +0700

    Time: 4 ms (29.86%), Space: 14.2 MB (33.85%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -6,16 +6,22 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root, targetSum):
+
+        # nếu cây rỗng → không có đường đi nào
         if not root:
             return False
-        
-        # nếu là node lá
+
+        # nếu gặp node lá (không có con trái và phải)
         if not root.left and not root.right:
+            # kiểm tra xem giá trị còn lại có đúng bằng node này không
             return targetSum == root.val
-        
-        # trừ dần target
+
+        # giảm targetSum đi giá trị của node hiện tại
         targetSum -= root.val
-        
-        return (self.hasPathSum(root.left, targetSum) or
-                self.hasPathSum(root.right, targetSum))
-        
+
+        # kiểm tra đệ quy:
+        # chỉ cần 1 trong 2 nhánh có đường đi hợp lệ là True
+        return (
+            self.hasPathSum(root.left, targetSum) or
+            self.hasPathSum(root.right, targetSum)
+        )
