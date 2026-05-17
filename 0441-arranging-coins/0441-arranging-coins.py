433cfcb672839644a0f9817284664c2eebe878ab
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:44:23 +0700

    Time: 399 ms (38.32%), Space: 12.3 MB (52.59%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,8 +1,18 @@
 class Solution:
     def arrangeCoins(self, n):
+        
+        # i là số coin cần cho hàng hiện tại (hàng 1, 2, 3, ...)
         i = 1
+        
+        # tiếp tục xây hàng khi còn đủ coin
         while n >= i:
+            
+            # dùng i coin để xây hàng hiện tại
             n = n - i
+            
+            # tăng số coin cần cho hàng tiếp theo
             i += 1
         
+        # i đã vượt quá số hàng xây được
+        # nên kết quả là i - 1
         return i - 1
