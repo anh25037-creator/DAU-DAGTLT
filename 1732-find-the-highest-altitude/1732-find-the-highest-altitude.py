1557f0789913e8c84896527c1bd78cc9d15021f1
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:50:36 +0700

    Time: 0 ms (100%), Space: 12.2 MB (90.32%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -1,11 +1,21 @@
 class Solution:
     def largestAltitude(self, gain):
+
+        # Độ cao hiện tại
         altitude = 0
-        max_altitude = 0
-        
+
+        # Độ cao lớn nhất
+        highest = 0
+
+        # Duyệt từng độ tăng/giảm độ cao
         for g in gain:
+
+            # Cập nhật độ cao hiện tại
             altitude += g
-            if altitude > max_altitude:
-                max_altitude = altitude
-                
-        return max_altitude
+
+            # Nếu độ cao hiện tại lớn hơn highest
+            if altitude > highest:
+                highest = altitude
+
+        # Trả về độ cao lớn nhất
+        return highest
