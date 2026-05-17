49c56009e0870ad528d44beb1418e4fe1a407d74
anh25037-creator <anh25037@gmail.com>
2026-05-17 14:59:27 +0700

    Time: 2 ms (1.64%), Space: 12.3 MB (84.84%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.py
+++ 1518-water-bottles/1518-water-bottles.py
@@ -1,13 +1,26 @@
 class Solution:
     def numWaterBottles(self, numBottles, numExchange):
+
+        # Tổng số chai nước đã uống
         total = 0
+
+        # Số chai rỗng hiện có
         empty = 0
-        
+
+        # Khi vẫn còn chai nước đầy
         while numBottles > 0:
-            total += numBottles      # uống hết chai đầy
-            empty += numBottles      # có thêm chai rỗng
-            
-            numBottles = empty // numExchange  # đổi được bao nhiêu chai mới
-            empty = empty % numExchange        # còn lại chai rỗng
-        
+
+            # Uống hết các chai hiện tại
+            total += numBottles
+
+            # Sau khi uống sẽ có chai rỗng
+            empty += numBottles
+
+            # Đổi chai rỗng lấy chai đầy mới
+            numBottles = empty // numExchange
+
+            # Số chai rỗng còn dư sau khi đổi
+            empty = empty % numExchange
+
+        # Trả về tổng số chai đã uống
         return total
