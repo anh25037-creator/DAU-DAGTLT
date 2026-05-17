7d8156ccfce3a50e8e7f97f43810d71e477da1ef
anh25037-creator <anh25037@gmail.com>
2026-05-17 13:48:01 +0700

    Time: 5 ms (81.05%), Space: 12.4 MB (57.56%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,5 +1,7 @@
 class Solution:
     def romanToInt(self, s):
+
+        # Dictionary lưu giá trị của từng ký tự La Mã
         values = {
             'I': 1,
             'V': 5,
@@ -10,11 +12,21 @@ class Solution:
             'M': 1000
         }
 
+        # Kết quả cuối cùng
         total = 0
+
+        # Duyệt từng ký tự trong chuỗi
         for i in range(len(s)):
-            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+
+            # Nếu ký tự hiện tại nhỏ hơn ký tự phía sau
+            # nghĩa là trường hợp trừ (IV, IX, XL,...)
+            if i < len(s) - 1 and values[s[i]] < values[s[i + 1]]:
+
+                # Trừ giá trị hiện tại
                 total -= values[s[i]]
+
             else:
+                # Ngược lại thì cộng bình thường
                 total += values[s[i]]
 
         return total
