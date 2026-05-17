661f6027f7a29c3673791c3452162362d194f2d5
anh25037-creator <anh25037@gmail.com>
2026-05-17 13:42:47 +0700

    Time: 0 ms (100%), Space: 12.3 MB (78.78%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,3 +1,11 @@
 class Solution:
     def lengthOfLastWord(self, s):
-        return len(s.strip().split()[-1])
+        
+        # Xóa khoảng trắng ở đầu và cuối chuỗi
+        s = s.strip()
+        
+        # Tách chuỗi thành các từ
+        words = s.split()
+        
+        # Lấy độ dài của từ cuối cùng
+        return len(words[-1])
