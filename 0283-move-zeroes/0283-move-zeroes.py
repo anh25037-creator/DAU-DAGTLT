e3f21a6ff98ea60d6785feedd178bd463e4cbbc0
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:25:54 +0700

    Time: 0 ms (100%), Space: 13.3 MB (97.49%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,8 +1,22 @@
 class Solution:
     def moveZeroes(self, nums):
+        
+        # k là vị trí để ghi các số khác 0
         k = 0
         
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[k] = nums[k], nums[i]
+        # duyệt toàn bộ mảng
+        for x in nums:
+            
+            # nếu gặp số khác 0
+            if x != 0:
+                
+                # đưa số đó lên vị trí k
+                nums[k] = x
+                
+                # tăng k
                 k += 1
+        
+        # sau khi đưa hết số khác 0 lên trước
+        # điền 0 vào phần còn lại
+        for i in range(k, len(nums)):
+            nums[i] = 0
