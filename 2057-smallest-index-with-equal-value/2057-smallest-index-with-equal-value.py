0adf5bd61cb6dacd1f15932a3a505e44ca3e5176
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:18:32 +0700

    Time: 0 ms (100%), Space: 12.4 MB (19.53%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.py
@@ -1,6 +1,14 @@
 class Solution:
     def smallestEqual(self, nums):
+
+        # Duyệt từng vị trí trong mảng
         for i in range(len(nums)):
+
+            # Kiểm tra i mod 10 có bằng nums[i] không
             if i % 10 == nums[i]:
+
+                # Trả về vị trí đầu tiên thỏa điều kiện
                 return i
+
+        # Nếu không tìm thấy
         return -1
