542e138eeb451a6d863283083fcc41ec48e668ba
anh25037-creator <anh25037@gmail.com>
2026-05-17 14:01:11 +0700

    Time: 26 ms (27.01%), Space: 13.1 MB (53.05%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.py
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -1,13 +1,21 @@
 class Solution:
     def findPoisonedDuration(self, timeSeries, duration):
-        if not timeSeries:
-            return 0
-        
+
+        # Tổng thời gian bị trúng độc
         total = 0
-        
+
+        # Duyệt đến phần tử kế cuối
         for i in range(len(timeSeries) - 1):
-            total += min(duration, timeSeries[i+1] - timeSeries[i])
-        
-        total += duration  # cộng lần cuối
-        
+
+            # Khoảng cách giữa 2 lần tấn công
+            gap = timeSeries[i + 1] - timeSeries[i]
+
+            # Nếu lần tấn công tiếp theo xảy ra
+            # trước khi độc hết
+            # thì chỉ cộng phần thời gian thực sự thêm vào
+            total += min(gap, duration)
+
+        # Cộng thêm duration của đòn cuối cùng
+        total += duration
+
         return total
