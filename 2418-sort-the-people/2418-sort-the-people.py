64cb78ec49643af95715ddc6ed1f2451e1f7100b
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:06:35 +0700

    Time: 7 ms (64.49%), Space: 12.7 MB (45.45%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,3 +1,21 @@
 class Solution:
     def sortPeople(self, names, heights):
-        return [x for _,x in sorted(zip(heights,names), reverse=True)]
+
+        # Ghép chiều cao với tên
+        people = []
+
+        for i in range(len(names)):
+            people.append([heights[i], names[i]])
+
+        # Sắp xếp giảm dần theo chiều cao
+        people.sort(reverse=True)
+
+        # Mảng kết quả
+        result = []
+
+        # Lấy tên sau khi sắp xếp
+        for height, name in people:
+            result.append(name)
+
+        # Trả về kết quả
+        return result
