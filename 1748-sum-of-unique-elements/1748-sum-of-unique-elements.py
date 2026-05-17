f9627a6c065c70533e6fc4fef312b4907272aae9
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:54:26 +0700

    Time: 0 ms (100%), Space: 12.4 MB (16.58%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -1,9 +1,17 @@
 class Solution:
     def sumOfUnique(self, nums):
+
+        # Biến lưu tổng các số xuất hiện đúng 1 lần
         total = 0
-        
+
+        # Duyệt từng phần tử trong mảng
         for i in nums:
+
+            # Nếu số i chỉ xuất hiện đúng 1 lần
             if nums.count(i) == 1:
+
+                # Cộng vào tổng
                 total += i
-                
+
+        # Trả về kết quả
         return total
