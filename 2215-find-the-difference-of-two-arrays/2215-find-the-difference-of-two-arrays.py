5d63f0d541e858df35d88d501af29da315d3d43f
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:09:54 +0700

    Time: 11 ms (44.86%), Space: 12.6 MB (94.39%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.py
@@ -1,7 +1,13 @@
 class Solution:
     def findDifference(self, nums1, nums2):
+        
+        # chuyển nums1 thành set để loại bỏ phần tử trùng lặp
+        # và tăng tốc độ tìm kiếm
         s1 = set(nums1)
+        
+        # chuyển nums2 thành set tương tự
         s2 = set(nums2)
         
+        # s1 - s2: các phần tử chỉ có trong nums1
+        # s2 - s1: các phần tử chỉ có trong nums2
         return [list(s1 - s2), list(s2 - s1)]
-        
