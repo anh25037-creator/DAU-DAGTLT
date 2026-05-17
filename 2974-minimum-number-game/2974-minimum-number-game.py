4dbaeeca1ec541459beac55f57f93cb20fb4be24
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:17:17 +0700

    Time: 0 ms (100%), Space: 12.2 MB (93.36%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.py
+++ 2974-minimum-number-game/2974-minimum-number-game.py
@@ -1,4 +1,22 @@
 class Solution:
     def numberGame(self, nums):
+        
+        # sắp xếp tăng dần để luôn lấy min dễ dàng
         nums.sort()
-        return [nums[i^1] for i in range(len(nums))]
+        
+        # kết quả cuối cùng
+        arr = []
+        
+        # duyệt từng cặp 2 phần tử
+        for i in range(0, len(nums), 2):
+            
+            # nums[i] là nhỏ hơn trong cặp
+            # nums[i+1] là lớn hơn trong cặp
+            
+            # Bob append trước → số lớn hơn đi trước
+            arr.append(nums[i + 1])
+            
+            # Alice append sau → số nhỏ hơn đi sau
+            arr.append(nums[i])
+        
+        return arr
