0e5bd2b5583bfa28edbe3cd2147614c1c8323318
anh25037-creator <anh25037@gmail.com>
2026-05-17 13:53:10 +0700

    Time: 35 ms (40.96%), Space: 13.5 MB (76.53%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.py
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -1,14 +1,27 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums):
-        max_count = 0
-        current_count = 0
-        
+
+        # Đếm số lượng số 1 liên tiếp hiện tại
+        count = 0
+
+        # Lưu kết quả lớn nhất
+        maximum = 0
+
+        # Duyệt từng phần tử trong mảng
         for num in nums:
+
+            # Nếu gặp số 1
             if num == 1:
-                current_count += 1
-                max_count = max(max_count, current_count)
+
+                # Tăng số lượng liên tiếp
+                count += 1
+
+                # Cập nhật giá trị lớn nhất
+                maximum = max(maximum, count)
+
             else:
-                current_count = 0
-                
-        return max_count
-        
+                # Nếu gặp số 0
+                # reset lại số lượng liên tiếp
+                count = 0
+
+        return maximum
