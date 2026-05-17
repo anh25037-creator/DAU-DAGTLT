2a284cf73b1026a52a9b47ce0d55d7494d2def27
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:05:07 +0700

    Time: 4 ms (61.37%), Space: 12.3 MB (96.57%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.py
@@ -1,5 +1,12 @@
 class Solution:
     def areOccurrencesEqual(self, s):
+
+        # Import Counter để đếm số lần xuất hiện
         from collections import Counter
+
+        # Đếm tần suất của từng ký tự
         freq = Counter(s)
+
+        # Lấy các giá trị tần suất và đưa vào set
+        # Nếu tất cả giống nhau thì set chỉ có 1 phần tử
         return len(set(freq.values())) == 1
