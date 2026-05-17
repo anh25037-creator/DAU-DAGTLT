a174b396f33928327692efb718e93b5f50205229
anh25037-creator <anh25037@gmail.com>
2026-05-17 14:55:07 +0700

    Time: 0 ms (100%), Space: 12.4 MB (55.81%) - LeetHub

--- 1051-height-checker/1051-height-checker.py
+++ 1051-height-checker/1051-height-checker.py
@@ -1,10 +1,20 @@
 class Solution:
     def heightChecker(self, heights):
+        
+        # Sắp xếp mảng heights để tạo thứ tự đúng
         expected = sorted(heights)
+
+        # Biến đếm số vị trí sai
         count = 0
-        
+
+        # Duyệt từng vị trí trong mảng
         for i in range(len(heights)):
+
+            # Nếu chiều cao hiện tại khác chiều cao mong đợi
             if heights[i] != expected[i]:
+
+                # Tăng biến đếm
                 count += 1
-        
+
+        # Trả về số vị trí sai
         return count
