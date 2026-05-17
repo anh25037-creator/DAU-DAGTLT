703c3cb2d0144290222094acba5ccce4aaa47efd
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:49:24 +0700

    Time: 34 ms (69.25%), Space: 23.7 MB (95.72%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,12 +1,20 @@
 class Solution:
     def containsNearbyDuplicate(self, nums, k):
-        index_map = {}
-        
-        for i in range(len(nums)):
-            if nums[i] in index_map:
-                if i - index_map[nums[i]] <= k:
-                    return True
-            
-            index_map[nums[i]] = i
-        
+        # dictionary để lưu: số -> vị trí xuất hiện gần nhất
+        last_index = {}
+
+        # duyệt mảng với cả chỉ số i và giá trị num
+        for i, num in enumerate(nums):
+
+            # nếu số này đã từng xuất hiện trước đó
+            if num in last_index:
+
+                # kiểm tra khoảng cách giữa 2 lần xuất hiện
+                if i - last_index[num] <= k:
+                    return True  # thỏa điều kiện -> trả về True
+
+            # cập nhật lại vị trí mới nhất của num
+            last_index[num] = i
+
+        # duyệt xong không tìm thấy cặp thỏa điều kiện
         return False
