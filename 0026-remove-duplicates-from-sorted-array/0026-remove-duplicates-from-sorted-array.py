e7ca0a87e3067e4a392e20b1705ead3961531f17
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:20:01 +0700

    Time: 7 ms (20.03%), Space: 13.1 MB (93.46%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,8 +1,21 @@
 class Solution:
     def removeDuplicates(self, nums):
+        
+        # k là vị trí để ghi phần tử unique tiếp theo
         k = 0
+        
+        # duyệt từng phần tử trong mảng
         for n in nums:
-            if k == 0 or n != nums[k-1]:
+            
+            # nếu đây là phần tử đầu tiên
+            # hoặc khác với phần tử cuối cùng đã lưu
+            if k == 0 or n != nums[k - 1]:
+                
+                # ghi n vào vị trí k
                 nums[k] = n
+                
+                # tăng vị trí lưu
                 k += 1
+        
+        # trả về số lượng phần tử unique
         return k
