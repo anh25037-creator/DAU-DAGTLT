7eb224a4e0668524a2b38873728c345e5ce04a22
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:29:17 +0700

    Time: 0 ms (100%), Space: 12.2 MB (89.86%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.py
@@ -1,10 +1,20 @@
 class Solution:
     def targetIndices(self, nums, target):
+
+        # Sắp xếp mảng tăng dần
         nums.sort()
+
+        # Danh sách lưu các vị trí có giá trị target
         result = []
 
+        # Duyệt từng vị trí trong mảng
         for i in range(len(nums)):
+
+            # Nếu phần tử tại vị trí i bằng target
             if nums[i] == target:
+
+                # Thêm index vào result
                 result.append(i)
 
+        # Trả về danh sách kết quả
         return result
