61eeceaad8d9684d10416b0e99fe79b48f78cb2d
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:52:07 +0700

    Time: 0 ms (100%), Space: 12.2 MB (89.94%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -1,8 +1,12 @@
 class Solution:
     def minimumSum(self, num):
+
+        # Chuyển số thành danh sách ký tự rồi sắp xếp tăng dần
         digits = sorted(str(num))
-        
+
+        # Tạo 2 số mới sao cho tổng nhỏ nhất
         new1 = int(digits[0] + digits[2])
         new2 = int(digits[1] + digits[3])
-        
+
+        # Trả về tổng
         return new1 + new2
