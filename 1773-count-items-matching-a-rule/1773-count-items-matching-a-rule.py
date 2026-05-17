f0972f8ca372d34f3452f12db0f0ed55cea0eb13
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:56:04 +0700

    Time: 0 ms (100%), Space: 16.6 MB (56.32%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.py
@@ -1,16 +1,25 @@
 class Solution:
     def countMatches(self, items, ruleKey, ruleValue):
+
+        # Biến đếm số item thỏa điều kiện
         count = 0
-        
+
+        # Xác định vị trí cần kiểm tra
         if ruleKey == "type":
             index = 0
+
         elif ruleKey == "color":
             index = 1
+
         else:
             index = 2
 
+        # Duyệt từng item
         for item in items:
+
+            # Nếu giá trị tại vị trí index bằng ruleValue
             if item[index] == ruleValue:
                 count += 1
 
+        # Trả về kết quả
         return count
