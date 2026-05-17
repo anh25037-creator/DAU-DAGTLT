877c77205593d4b3a57b49ec0e59e9fe886d7c40
anh25037-creator <anh25037@gmail.com>
2026-05-17 20:38:45 +0700

    Time: 0 ms (100%), Space: 33.1 MB (50.09%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,16 +1,17 @@
-class Solution(object):
+class Solution:
     def findCenter(self, edges):
-        """
-        :type edges: List[List[int]]
-        :rtype: int
-        """
 
-        a, b = edges[0]
-        c, d = edges[1]
+        # Lấy 2 cạnh đầu tiên của đồ thị
+        # Vì trong star graph, node trung tâm sẽ xuất hiện trong tất cả các cạnh
+        a, b = edges[0]  # cạnh thứ nhất
+        c, d = edges[1]  # cạnh thứ hai
 
-        # node chung giữa 2 cạnh đầu
+        # Kiểm tra node nào xuất hiện ở cả 2 cạnh
+        # Vì center node chắc chắn nằm trong cả edges[0] và edges[1]
+
         if a == c or a == d:
+            # nếu a xuất hiện trong cạnh thứ 2 → a là center
             return a
 
+        # nếu không phải a thì b chắc chắn là center
         return b
-        
