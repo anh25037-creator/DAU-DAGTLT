f14d2672b14a141ee84e0cce0930cf4fa10ae50d
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:15:51 +0700

    Time: 12 ms (89.15%), Space: 12.4 MB (93.8%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -1,9 +1,22 @@
-import math
+import heapq
 
 class Solution:
     def pickGifts(self, gifts, k):
+        
+        # tạo max heap bằng số âm
+        heap = [-g for g in gifts]
+        heapq.heapify(heap)
+        
         for _ in range(k):
-            m = max(gifts)
-            i = gifts.index(m)
-            gifts[i] = int(math.sqrt(m))
-        return sum(gifts)
+            
+            # lấy phần tử lớn nhất
+            largest = -heapq.heappop(heap)
+            
+            # tính floor(sqrt)
+            reduced = int(largest ** 0.5)
+            
+            # đẩy lại vào heap
+            heapq.heappush(heap, -reduced)
+        
+        # tính tổng còn lại
+        return -sum(heap)
