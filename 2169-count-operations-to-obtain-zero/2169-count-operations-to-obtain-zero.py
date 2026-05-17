765470769959fd633a97343e3501dc6e1fef150e
anh25037-creator <anh25037@gmail.com>
2026-05-17 17:00:00 +0700

    Time: 44 ms (69.01%), Space: 12.3 MB (86.2%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.py
@@ -1,10 +1,24 @@
 class Solution:
     def countOperations(self, num1, num2):
+
+        # Biến đếm số thao tác
         count = 0
+
+        # Lặp khi cả num1 và num2 khác 0
         while num1 and num2:
+
+            # Nếu num1 lớn hơn num2
             if num1 > num2:
+
+                # Lấy num1 trừ num2
                 num1 -= num2
+
             else:
+                # Ngược lại lấy num2 trừ num1
                 num2 -= num1
+
+            # Tăng số lần thao tác
             count += 1
+
+        # Trả về kết quả
         return count
