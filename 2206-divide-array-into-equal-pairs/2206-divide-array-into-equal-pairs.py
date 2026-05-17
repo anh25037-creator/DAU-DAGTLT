6b45f48131e56a5e53eeff23a8080159dd046560
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:06:30 +0700

    Time: 2 ms (91.28%), Space: 12.6 MB (7.34%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.py
@@ -1,11 +1,19 @@
 class Solution:
     def divideArray(self, nums):
+        
+        # import Counter để đếm tần suất các phần tử trong mảng
         from collections import Counter
         
+        # đếm số lần xuất hiện của từng phần tử
         count = Counter(nums)
         
+        # duyệt qua tất cả giá trị tần suất
         for v in count.values():
+            
+            # nếu có phần tử xuất hiện số lần lẻ
+            # thì không thể chia thành các cặp
             if v % 2 != 0:
                 return False
         
+        # nếu tất cả đều xuất hiện chẵn lần
         return True
