c160db66b73bb8d57add937d8cfcbcf48625f169
anh25037-creator <anh25037@gmail.com>
2026-05-17 18:11:13 +0700

    Time: 3 ms (90.51%), Space: 12.5 MB (83.2%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
@@ -1,9 +1,17 @@
 class Solution:
     def findClosestNumber(self, nums):
+        
+        # giả sử phần tử đầu tiên là đáp án tạm thời
         ans = nums[0]
         
+        # duyệt từng phần tử trong mảng
         for x in nums:
+            
+            # so sánh theo 2 tiêu chí:
+            # 1. |x| nhỏ hơn |ans| → x gần 0 hơn
+            # 2. nếu |x| bằng nhau → chọn số lớn hơn
             if abs(x) < abs(ans) or (abs(x) == abs(ans) and x > ans):
                 ans = x
                 
+        # trả về số gần 0 nhất (theo điều kiện đề bài)
         return ans
