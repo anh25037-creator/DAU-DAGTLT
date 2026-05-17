49275f9b2d95b48c6f949145ca1127e2d298699d
anh25037-creator <anh25037@gmail.com>
2026-05-17 16:31:52 +0700

    Time: 0 ms (100%), Space: 12.3 MB (92.76%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
@@ -1,9 +1,20 @@
 class Solution:
     def mostWordsFound(self, sentences):
+
+        # Biến lưu số lượng từ lớn nhất
         max_words = 0
 
-        for s in sentences:
-            words = s.split()
-            max_words = max(max_words, len(words))
+        # Duyệt từng câu
+        for sentence in sentences:
 
+            # Tách câu thành các từ
+            words = sentence.split()
+
+            # Đếm số từ trong câu
+            count = len(words)
+
+            # Cập nhật giá trị lớn nhất
+            max_words = max(max_words, count)
+
+        # Trả về kết quả
         return max_words
