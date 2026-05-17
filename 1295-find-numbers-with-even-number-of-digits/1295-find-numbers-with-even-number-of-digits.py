d807cdcd9e91dd72eff663f16ac50461ca56cf75
anh25037-creator <anh25037@gmail.com>
2026-05-17 15:07:57 +0700

    Time: 3 ms (54.49%), Space: 12.2 MB (96.54%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.py
@@ -1,9 +1,18 @@
 class Solution:
     def findNumbers(self, nums):
+
+        # Biến đếm số phần tử có số chữ số chẵn
         count = 0
-        
+
+        # Duyệt từng số trong mảng
         for num in nums:
-            if len(str(num)) % 2 == 0:
+
+            # Chuyển số thành chuỗi để đếm số chữ số
+            digits = len(str(num))
+
+            # Nếu số chữ số là chẵn
+            if digits % 2 == 0:
                 count += 1
-                
+
+        # Trả về kết quả
         return count
