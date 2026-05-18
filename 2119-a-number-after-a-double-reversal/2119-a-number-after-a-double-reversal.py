27de200f361f1eb8c3ce224f0eefe854e7cc7e33
anh25037-creator <anh25037@gmail.com>
2026-05-18 11:27:42 +0700

    Time: 0 ms (100%), Space: 12.3 MB (54.6%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.py
@@ -1,20 +1,22 @@
-class Solution(object):
+class Solution:
     def isSameAfterReversals(self, num):
-        """
-        :type num: int
-        :rtype: bool
-        """
-
-        # hàm đảo số
+        # hàm đảo ngược một số nguyên
         def reverse(x):
             rev = 0
+
+            # lặp cho đến khi hết chữ số
             while x > 0:
-                rev = rev * 10 + x % 10
-                x //= 10
+                digit = x % 10        # lấy chữ số cuối
+                rev = rev * 10 + digit  # ghép vào số đảo
+                x //= 10              # bỏ chữ số cuối
+
             return rev
 
+        # bước 1: đảo lần 1
         reversed1 = reverse(num)
+
+        # bước 2: đảo lần 2
         reversed2 = reverse(reversed1)
 
+        # so sánh kết quả
         return reversed2 == num
-        
