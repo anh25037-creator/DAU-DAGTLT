574f2945bba475157231c86616cfec3c9888309a
anh25037-creator <anh25037@gmail.com>
2026-05-18 18:37:14 +0700

    Time: 0 ms (100%), Space: 12.4 MB (53.16%) - LeetHub

--- 3099-harshad-number/3099-harshad-number.py
+++ 3099-harshad-number/3099-harshad-number.py
@@ -5,16 +5,30 @@ class Solution(object):
         :rtype: int
         """
 
+        # total dùng để lưu tổng các chữ số
         total = 0
+
+        # temp là biến phụ để tách từng chữ số
+        # giữ nguyên x ban đầu để còn kiểm tra
         temp = x
 
-        # tính tổng chữ số
+        # lặp cho đến khi temp = 0
         while temp > 0:
+
+            # temp % 10 lấy chữ số cuối cùng
+            # ví dụ: 172 % 10 = 2
             total += temp % 10
+
+            # temp //= 10 để bỏ chữ số cuối
+            # ví dụ: 172 // 10 = 17
             temp //= 10
 
-        # kiểm tra Harshad
+        # kiểm tra số Harshad
+        # số Harshad là số chia hết cho tổng chữ số của nó
         if x % total == 0:
+
+            # nếu chia hết → trả về tổng chữ số
             return total
 
+        # nếu không chia hết → không phải Harshad
         return -1
