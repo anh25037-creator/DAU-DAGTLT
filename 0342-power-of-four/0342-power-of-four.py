231baeff2005c7bcf984b992bcea1c52cc1ad331
anh25037-creator <anh25037@gmail.com>
2026-05-18 18:12:43 +0700

    Time: 0 ms (100%), Space: 12.3 MB (54.01%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -5,10 +5,14 @@ class Solution(object):
         :rtype: bool
         """
 
+        # số <= 0 không phải lũy thừa của 4
         if n <= 0:
             return False
 
+        # chia liên tục cho 4
         while n % 4 == 0:
             n //= 4
 
+        # nếu cuối cùng còn 1
+        # thì là lũy thừa của 4
         return n == 1
