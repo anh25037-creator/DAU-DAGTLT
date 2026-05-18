91fa10e493951584790383da496f8e17adb54b1b
anh25037-creator <anh25037@gmail.com>
2026-05-18 11:26:24 +0700

    Time: 19 ms (55.1%), Space: 12.3 MB (53.21%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,19 +1,18 @@
-class Solution(object):
+class Solution:
     def fib(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-
         # trường hợp cơ bản
         if n == 0:
             return 0
         if n == 1:
             return 1
 
-        a, b = 0, 1  # F(0), F(1)
+        # f0 = F(0), f1 = F(1)
+        f0, f1 = 0, 1
 
-        for _ in range(2, n + 1):
-            a, b = b, a + b  # cập nhật 2 số liên tiếp
+        # tính từ F(2) đến F(n)
+        for i in range(2, n + 1):
+            # lưu giá trị mới = tổng 2 số trước
+            f0, f1 = f1, f0 + f1
 
-        return b
+        # f1 là kết quả cuối cùng F(n)
+        return f1
