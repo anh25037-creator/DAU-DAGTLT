67e63b6a8ae4579d329a86f7fa62528e067e1bd9
anh25037-creator <anh25037@gmail.com>
2026-05-18 18:35:08 +0700

    Time: 28 ms (82.77%), Space: 12.3 MB (61%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.py
+++ 2652-sum-multiples/2652-sum-multiples.py
@@ -5,11 +5,16 @@ class Solution(object):
         :rtype: int
         """
 
+        # lưu tổng kết quả
         total = 0
 
+        # duyệt từ 1 -> n
         for i in range(1, n + 1):
 
+            # nếu chia hết cho 3 hoặc 5 hoặc 7
             if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
+
+                # cộng vào tổng
                 total += i
 
         return total
