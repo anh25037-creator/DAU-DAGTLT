b2c8207a4bc903f65a2201801e2cd67889a6edb4
anh25037-creator <anh25037@gmail.com>
2026-05-18 18:26:28 +0700

    Time: 3 ms (25%), Space: 12.4 MB (13.89%) - LeetHub

--- 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
+++ 2224-minimum-number-of-operations-to-convert-time/2224-minimum-number-of-operations-to-convert-time.py
@@ -6,21 +6,38 @@ class Solution(object):
         :rtype: int
         """
 
-        # đổi HH:MM -> phút
+        # hàm đổi thời gian HH:MM -> tổng số phút
         def to_minutes(t):
+
+            # tách giờ và phút
+            # ví dụ "02:30" -> ["02", "30"]
             h, m = map(int, t.split(":"))
+
+            # đổi tất cả sang phút
+            # 2 giờ 30 phút = 2*60 + 30
             return h * 60 + m
 
+        # đổi current sang phút
         cur = to_minutes(current)
+
+        # đổi correct sang phút
         cor = to_minutes(correct)
 
+        # số phút cần tăng thêm
         diff = cor - cur
 
+        # biến đếm số thao tác
         ops = 0
 
-        # greedy
+        # dùng greedy:
+        # ưu tiên bước lớn trước
         for step in [60, 15, 5, 1]:
+
+            # lấy được bao nhiêu lần step
             ops += diff // step
+
+            # cập nhật số phút còn lại
             diff %= step
 
+        # trả về số thao tác tối thiểu
         return ops
