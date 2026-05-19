a1b2e2a2fb0e8cc1bf1d16cfc4bace0df4053dcc
anh25037-creator <anh25037@gmail.com>
2026-05-19 10:22:35 +0700

    Time: 3 ms (41.13%), Space: 12.5 MB (18.09%) - LeetHub

--- 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.py
@@ -5,23 +5,32 @@ class Solution(object):
         :rtype: bool
         """
 
+        # lưu tổng các số có 1 chữ số
         single_sum = 0
+
+        # lưu tổng các số có 2 chữ số
         double_sum = 0
 
+        # duyệt từng phần tử trong mảng nums
         for num in nums:
 
-            # số 1 chữ số
+            # nếu là số 1 chữ số (0 -> 9)
             if num < 10:
                 single_sum += num
 
-            # số 2 chữ số
+            # nếu là số 2 chữ số (10 -> 99)
             elif 10 <= num < 100:
                 double_sum += num
 
+        # tính tổng tất cả phần tử trong mảng
         total = sum(nums)
+
+        # Alice thắng nếu:
+        # - chọn nhóm số 1 chữ số mà tổng lớn hơn phần còn lại
+        # HOẶC
+        # - chọn nhóm số 2 chữ số mà tổng lớn hơn phần còn lại
 
         return (
             single_sum > total - single_sum or
             double_sum > total - double_sum
         )
-        
