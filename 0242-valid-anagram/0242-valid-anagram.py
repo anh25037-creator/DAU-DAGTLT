566500d8e05454f20a67c90640b839b627720f14
anh25037-creator <anh25037@gmail.com>
2026-05-20 17:23:40 +0700

    Time: 23 ms (50.34%), Space: 13.4 MB (29.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,13 @@
+#Kiểm tra xem hai chuỗi có phải hoán vị của nhau hay không.
 class Solution:
     def isAnagram(self, s, t):
-        
-        # Nếu độ dài khác nhau
-        # chắc chắn không phải anagram
-        if len(s) != len(t):
-            return False
-        
-        # Sắp xếp 2 chuỗi rồi so sánh
+
+        # sorted() dùng để sắp xếp ký tự theo alphabet
+        # Ví dụ:
+        # sorted("rat") -> ['a','r','t']
+        # sorted("tar") -> ['a','r','t']
+
+        # Nếu sau khi sắp xếp mà giống nhau
+        # => hai chuỗi có cùng ký tự và cùng số lượng ký tự
+        # => là anagram
         return sorted(s) == sorted(t)
