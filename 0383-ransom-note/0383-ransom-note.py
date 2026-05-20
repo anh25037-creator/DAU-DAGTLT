1d066bc43185cc8e4f1cc1d8d7ef292aa86c9550
anh25037-creator <anh25037@gmail.com>
2026-05-20 22:24:16 +0700

    Time: 27 ms (79.19%), Space: 12.5 MB (99.04%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,24 +1,42 @@
+# Kiểm tra xem ransomNote có thể tạo từ magazine hay không
 class Solution:
+
     def canConstruct(self, ransomNote, magazine):
-        
-        # Dictionary để đếm số lần xuất hiện ký tự trong magazine
+
+        # Dictionary dùng để lưu:
+        # ký tự : số lần xuất hiện
         count = {}
-        
+
         # Duyệt từng ký tự trong magazine
         for ch in magazine:
-            
-            # Tăng số lần xuất hiện
-            count[ch] = count.get(ch, 0) + 1
-        
+
+            # Nếu ký tự đã tồn tại trong dictionary
+            if ch in count:
+
+                # Tăng số lần xuất hiện lên 1
+                count[ch] += 1
+
+            # Nếu ký tự chưa tồn tại
+            else:
+
+                # Thêm ký tự vào dictionary
+                # với số lượng ban đầu là 1
+                count[ch] = 1
+
         # Duyệt từng ký tự trong ransomNote
         for ch in ransomNote:
-            
-            # Nếu ký tự không có
-            # hoặc đã dùng hết
+
+            # Nếu ký tự không tồn tại trong dictionary
+            # hoặc số lượng còn lại bằng 0
             if ch not in count or count[ch] == 0:
+
+                # Không thể tạo ransomNote
                 return False
-            
+
             # Dùng 1 ký tự
+            # nên giảm số lượng đi 1
             count[ch] -= 1
-        
+
+        # Nếu duyệt hết mà không lỗi
+        # nghĩa là tạo được ransomNote
         return True
