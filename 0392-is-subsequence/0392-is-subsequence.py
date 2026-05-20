7593ebdee6ea590713a7b986e9e47fe84d861851
anh25037-creator <anh25037@gmail.com>
2026-05-20 20:17:34 +0700

    Time: 4 ms (21.09%), Space: 12.4 MB (48.35%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,22 +1,27 @@
-
 class Solution:
     def isSubsequence(self, s, t):
-        
-        # i dùng để theo dõi vị trí trong chuỗi s
-        i = 0
-        
-        # Duyệt từng ký tự trong chuỗi t
-        for ch in t:
-            
-            # Nếu:
-            # - i vẫn còn trong phạm vi của s
-            # - ký tự hiện tại của s bằng ký tự trong t
-            if i < len(s) and s[i] == ch:
-                
-                # Sang ký tự tiếp theo của s
-                i += 1
-        
-        # Nếu i bằng độ dài s
-        # nghĩa là đã tìm được toàn bộ ký tự của s theo đúng thứ tự
-        return i == len(s)
 
+        # i dùng để duyệt chuỗi s
+        i = 0
+
+        # j dùng để duyệt chuỗi t
+        j = 0
+
+        # lặp khi:
+        # - vẫn còn ký tự trong s
+        # - và vẫn còn ký tự trong t
+        while i < len(s) and j < len(t):
+
+            # nếu ký tự hiện tại của s
+            # giống ký tự hiện tại của t
+            if s[i] == t[j]:
+
+                # chuyển sang ký tự tiếp theo của s
+                i += 1
+
+            # luôn chuyển sang ký tự tiếp theo của t
+            j += 1
+
+        # nếu i đi hết chuỗi s
+        # nghĩa là đã tìm đủ các ký tự của s theo đúng thứ tự
+        return i == len(s)
